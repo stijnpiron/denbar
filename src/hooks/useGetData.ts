db5fc10b05e7942e8af7cba33ce9e5e503b5238a
Stijn Piron <stijn.piron@gmail.com>
2021-06-30 15:50:14 +0200

    firestore added, read and write is working

---
+++ src/hooks/useGetData.ts
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+import { useEffect, useState } from 'react';
+export const useGetData = (collection: string) => {
+  const [documents, setDocuments] = useState<any[]>([]);
+  const db = firebase.firestore();
+
+  useEffect(() => {
+    db.collection(collection)
+      .get()
+      .then((qs) => {
+        let arr: any[] = [];
+        qs.docs.map((d) => arr.push({ id: d.id, value: d.data() }));
+        setDocuments(arr);
+      });
+  }, [collection, db]);
+  return [documents];
+};
--- src/pages/orders/OrdersPage.tsx
+++ src/pages/orders/OrdersPage.tsx
@@ -1,7 +1,83 @@
+import { Button, ButtonGroup, createStyles, IconButton, makeStyles, Theme } from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+import firebase from 'firebase';
+import { useGetData } from 'hooks/useGetData';
+import { Redirect } from 'react-router-dom';
+
 interface OrdersPageProps {
   [key: string]: any;
 }
 
-const OrdersPage: React.FC<OrdersPageProps> = ({ period, dataProps }) => <>Admin page</>;
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    li: {
+      margin: 'auto',
+      display: 'flex',
+      flexFlow: 'row nowrap',
+      alignItems: 'center',
+    },
+  })
+);
+
+// TODO: remove POC code when implementing real functionality
+const makeid = (length: number = 10) => {
+  var result = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
+
+const makePrice = () => (Math.floor(Math.random() * 250) + 1) / 100;
+
+const OrdersPage: React.FC<OrdersPageProps> = ({ adminAuth }) => {
+  const classes = useStyles();
+
+  const [products] = useGetData('Products');
+  const db = firebase.firestore();
+
+  // TOOD: figure out how to auto refresh the data displayed on screen when adding an item
+  const handleAddItem = () => {
+    console.log(db.collection('Products').get());
+    db.collection('Products')
+      .add({
+        name: makeid(),
+        price: makePrice(),
+      })
+      .then(function () {
+        console.log('Value successfully written!');
+      })
+      .catch(function (error) {
+        console.error('Error writing Value: ', error);
+      });
+  };
+
+  return (
+    <>
+      {!adminAuth && <Redirect to="/admin" />}
+      <Button onClick={handleAddItem}>Add item</Button>
+      <ul>
+        {products.map((p) => (
+          <li className={classes.li}>
+            <div>
+              {p.value.name}: €{p.value.price}
+            </div>
+            <ButtonGroup variant="outlined" aria-label="outlined primary button group">
+              <IconButton color="primary">
+                <EditIcon />
+              </IconButton>
+              <IconButton color="secondary">
+                <DeleteIcon />
+              </IconButton>
+            </ButtonGroup>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
 export default OrdersPage;
